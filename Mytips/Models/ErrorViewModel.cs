6c5cb1844e6fa14556b9f81f7137168686d34437
dhk <unmix@nate.com>
2020-02-07 22:00:29 +0900

    초기 커밋.

---
+++ Mytips/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mytips.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
